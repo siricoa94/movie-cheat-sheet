d2d991439b63115ddd45aa0dee52c09d3e88a90d
Jenks <mikedjenk@gmail.com>
2019-07-14 23:44:41 -0400

    bareAjax

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $("#sub").on("click", function () {
-
+  event.preventDefault();
 
   var movieName = $("#exampleFormControlInput1").val().toLowerCase().trim();
 
@@ -8,7 +8,8 @@ $("#sub").on("click", function () {
   $("tbody").empty();
 
   var input = movieName
-  var queryURL = "https://www.omdbapi.com/?t=" + input + "&apikey=trilogy";
+  var queryURL = "https://www.omdbapi.com/?t=" + input +  "&plot=full&apikey=trilogy"; //||
+  //"https://www.omdbapi.com/?t=" + input + "&y=" + input + "&apikey=trilogy";
 
   console.log(input);
 
@@ -23,47 +24,48 @@ $("#sub").on("click", function () {
     var ratedTD = $("<td>").text(response.Rated);
     var plotTd = $("<td>").text(response.Plot);
     var actorTd = $("<td>").text(response.Actors);
-    var ratingsTd =$("<td>").text(response.Ratings[1].Value);
-    ratingsTd.append(response.Ratings[2].Value);
+    var ratingsTd =$("<td>").text(response.Ratings[0].Value);
+    // ratingsTd.append(response.Ratings[2].Value);
     var imgURL = response.Poster;
     var image = $("<img>").attr("src", imgURL)
-    tRow.append(image, titleTd, yearTd, ratingsTd, plotTd, ratedTD, actorTd);
+    tRow.append(image, titleTd, yearTd, ratingsTd, 
+      plotTd, ratedTD, actorTd);
     $("tbody").append(tRow);
 
   });
 
-  var tvURL = "http://api.tvmaze.com/search/shows?q=" + input;
+  // var tvURL = "http://api.tvmaze.com/search/shows?q=" + input;
 
-  $.ajax({
-    url: tvURL,
-    method: "GET"
-  }).then(function (telvision) {
-    console.log(telvision);
-    var tvRow = $("<tr>");
-    var nameTD = $("<td>")
-    tvRow.append(nameTD)
-    $("tbody").apend(tvRow);
-  });
-
-
+  // $.ajax({
+  //   url: tvURL,
+  //   method: "GET"
+  // }).then(function (telvision) {
+  //   console.log(telvision);
+  //   var tvRow = $("<tr>");
+  //   var nameTD = $("<td>")
+  //   tvRow.append(nameTD)
+  //   $("tbody").apend(tvRow);
+  // });
 
 
-  var giphyurl = "https://api.giphy.com/v1/gifs/search?q=" + input +
-    "&api_key=LgLbsRdUWao0JhCQMJryh9mbJgxU03D8&limit=5";
-  console.log(input);
-
-  $.ajax({
-    url: giphyurl,
-    method: "GET"
-  }).then(function (memes) {
-    console.log(memes);
-    var funtRow = $('<div>');
-    var gifsImage = $('<img>');
-    gifsImage.attr('src', memes.images.fixed_height.url);
-    gifsImage.addClass('gif');
-    $('#giphy').prepend(funtRow);
 
 
-  })
+  // var giphyurl = "https://api.giphy.com/v1/gifs/search?q=" + input +
+  //   "&api_key=LgLbsRdUWao0JhCQMJryh9mbJgxU03D8&limit=5";
+  // console.log(input);
+
+  // $.ajax({
+  //   url: giphyurl,
+  //   method: "GET"
+  // }).then(function (memes) {
+  //   console.log(memes);
+  //   var funtRow = $('<div>');
+  //   var gifsImage = $('<img>');
+  //   gifsImage.attr('src', memes.images.fixed_height.url);
+  //   gifsImage.addClass('gif');
+  //   $('#giphy').prepend(funtRow);
+
+
+  // })
 
 });
