be5ab07fd52f58053a639ee105aa31e4d5932523
Jenks <mikedjenk@gmail.com>
2019-07-15 22:08:25 -0400

    workingBackend

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
-$("#sub").on("click", function (event) {
-  event.preventDefault();
+$(document).on("keypress", function (event) {
+  if (event.which == 13) {
+    event.preventDefault();
+    $("tr").remove();
+    $('#gifyImageLoader').empty();
+    omdb();
+    giphy();
+  }
+});
 
-  var movieName = $("#exampleFormControlInput1").val().toLowerCase().trim();
 
 
 
-  $("tbody").empty();
 
-  var input = movieName
-  var queryURL = "https://www.omdbapi.com/?t=" + input + "&plot=full&apikey=trilogy" //&&
-  //"https://www.omdbapi.com/?t=" + input + "&y=" + input + "&apikey=trilogy";
+var input = $("#exampleFormControlInput1").val().toLowerCase().trim();
+console.log(input)
+var queryURL = "https://www.omdbapi.com/?t=" + input + "&plot=full&apikey=trilogy";
+var giphyurl = "https://api.giphy.com/v1/gifs/search?q=" + input +
+  "&api_key=LgLbsRdUWao0JhCQMJryh9mbJgxU03D8&limit=1";
+$("#sub").on("click", function () {
+  $("tr").remove();
+  $('#gifyImageLoader').empty();
+  omdb();
+  giphy();
+});
 
-  console.log(input);
+
+function omdb() {
+  var InfoHeading = $("<tr>")
+  var pHead = $("<th scope='col' id='tableFirst'>").text("Poster");
+  var tHead = $("<th scope='col' id='tableLast'>").text("Title");
+  var yHead = $("<th scope='col' id='tableHandle'>").text("Premiered");
+  var iHead = $("<th scope='col' id='tableHandle'>").text("IMDB Rating")
+  var plHead = $("<th scope='col' id='tableHandle'>").text("Plot Summary")
+  var rHead = $("<th scope='col' id='tableHandle'>").text("Rating")
+  var sHead = $("<th scope='col' id='tableHandle'>").text("Stars")
+  InfoHeading.append(pHead, tHead, yHead, iHead, plHead, rHead, sHead);
+  $(".table").append(InfoHeading);
+
+
+
+
+
+
+  var input = $("#exampleFormControlInput1").val().toLowerCase().trim();
+
+  var queryURL = "https://www.omdbapi.com/?t=" + input + "&plot=full&apikey=trilogy";
 
   $.ajax({
     url: queryURL,
@@ -19,7 +52,7 @@ $("#sub").on("click", function (event) {
   }).then(function (response) {
     console.log(response)
     var tRow = $("<tr>");
-    var titleTd = $("<td>").text(response.Title);
+    var titleTd = $("<td class='Info'>").text(response.Title);
     var yearTd = $("<td>").text(response.Released);
     var ratedTD = $("<td>").text(response.Rated);
     var plotTd = $("<td>").text(response.Plot);
@@ -29,56 +62,44 @@ $("#sub").on("click", function (event) {
     var image = $("<img>").attr("src", imgURL)
     tRow.append(image, titleTd, yearTd, ratingsTd,
       plotTd, ratedTD, actorTd);
-    $("tbody").append(tRow);
+    $(".table").append(tRow);
 
   });
-});
-  $("#sub").on("click", function (event) {
-    event.preventDefault();
-    
-    var Giphycall = $("#exampleFormControlInput1").val().toLowerCase().trim();
-    
-    
-    
-    var input = Giphycall
-    var giphyurl = "https://api.giphy.com/v1/gifs/search?q=" + input +
-      "&api_key=LgLbsRdUWao0JhCQMJryh9mbJgxU03D8&limit=5";
-    console.log(input);
+};
 
-    $.ajax({
-      url: giphyurl,
-      method: "GET"
-    }).then(function (memes) {
-      console.log(memes.data);
-      var results = memes.data
-      console.log(results);
-      for (var i = 0; i < results.length; i++){
+function giphy() {
+  var input = $("#exampleFormControlInput1").val().toLowerCase().trim();
+
+  var giphyurl = "https://api.giphy.com/v1/gifs/search?q=" + input +
+    "&api_key=LgLbsRdUWao0JhCQMJryh9mbJgxU03D8&limit=1";
+  console.log(input);
+
+  $.ajax({
+    url: giphyurl,
+    method: "GET"
+  }).then(function (memes) {
+    console.log(memes.data);
+    var results = memes.data
+    console.log(results);
+    for (var i = 0; i < results.length; i++) {
       var fun = $('<div>');
       var gifsImage = $('<img>');
       gifsImage.attr('src', results[i].images.fixed_height.url);
       gifsImage.attr("data-animate", results[i].images.fixed_height.url)
       gifsImage.addClass('gif');
+
       fun.append(gifsImage);
+
       $('#gifyImageLoader').prepend(fun);
-      
-      };
 
-    });
-    
+    };
+
   });
-  
-  // var tvURL = "http://api.tvmaze.com/search/shows?q=" + input;
 
-  // $.ajax({
-  //   url: tvURL,
-  //   method: "GET"
-  // }).then(function (telvision) {
-  //   console.log(telvision);
-  //   var tvRow = $("<tr>");
-  //   var nameTD = $("<td>")
-  //   tvRow.append(nameTD)
-  //   $("tbody").apend(tvRow);
-  // });
+}
+
+
+
 
 
 
